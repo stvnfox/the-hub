ba5ad57f95f7abb704e8b16dd990155c37a94d68
Steven <steven@stvnx.nl>
2023-12-01 20:44:09 +0100

    The Hub > Add The Hub light mode logo + added light mode primary colors

--- components/AuthenticationPageBase.vue
+++ components/AuthenticationPageBase.vue
@@ -1,8 +1,11 @@
 <template>
     <section class="container flex items-center justify-center gap-8 min-h-screen">
-        <div class="w-1/2">
-            <h1 class="text-3xl mb-2">The Hub</h1>
-            <p>Where all the cool kids hang out</p>
+        <div class="w-1/2 flex justify-center">
+            <img
+                class="w-80"
+                src="../assets/images/logo-light.svg"
+                alt="The Hub"
+            />
         </div>
         <div class="w-1/2">
             <slot name="form" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
             container: {
                 center: true,
             },
+            colors: {
+                purple: {
+                    DEFAULT: "",
+                    100: "#b3aOca",
+                    300: "#997fb8",
+                    500: "#7f5ea6",
+                    700: "#664a87",
+                },
+            },
         },
     },
     plugins: [],
