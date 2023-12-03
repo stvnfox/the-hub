66ef459495a6f505809c271708972a37a691d580
Steven <steven@stvnx.nl>
2023-12-03 20:23:16 +0100

    Added an example function to submit projects + add createUuid function

--- components/TheHub/TheHub.vue
+++ components/TheHub/TheHub.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+    import { createUuid } from "~/helpers/uuid"
+
     const user = useSupabaseUser()
     const { $client } = useNuxtApp()
 
     const getData = async () => {
         const { data } = await $client.projects.get.useQuery()
         console.log(data.value)
+    }
+
+    const addTestProject = async () => {
+        try {
+            await $client.projects.create.mutate({
+                id: createUuid(),
+                name: "This is a test project",
+                created_at: new Date().toISOString(),
+                owner_id: user.value?.id,
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     onMounted(() => {
@@ -17,6 +32,7 @@
         <sidebar-component />
         <div class="container flex-grow">
             <navigation-component />
+            <button @click="addTestProject">Add test project</button>
             <pre wrap>{{ user }}</pre>
         </div>
     </section>
